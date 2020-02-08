63dd258781fb67a4a065139b257f6972cab40d7e
unknown <thanhson16198@gmail.com>
2020-02-09 02:21:02 +0800

    Set up sample app.py

---
+++ practice_backend/app.py
@@ -0,0 +1,16 @@
+from starlette.applications import Starlette
+from starlette.responses import UJSONResponse
+from starlette.routing import Route
+import uvicorn
+
+
+async def homepage(request):
+    return UJSONResponse({'hello': 'world'})
+
+
+app = Starlette(debug=True, routes=[
+    Route('/', homepage),
+])
+
+if __name__ == "__main__":
+    uvicorn.run("practice_backend:app", host="127.0.0.1", port=8000, http='h11', loop='asyncio')
