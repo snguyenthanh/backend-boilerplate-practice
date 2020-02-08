df52319045750e09b12150cc9b49552d2d76b86e
unknown <thanhson16198@gmail.com>
2020-02-09 02:27:31 +0800

    Reformatted with Black

--- practice_backend/app.py
+++ practice_backend/app.py
@@ -5,12 +5,12 @@ import uvicorn
 
 
 async def homepage(request):
-    return UJSONResponse({'hello': 'world'})
+    return UJSONResponse({"hello": "world"})
 
 
-app = Starlette(debug=True, routes=[
-    Route('/', homepage),
-])
+app = Starlette(debug=True, routes=[Route("/", homepage),])
 
 if __name__ == "__main__":
-    uvicorn.run("practice_backend:app", host="127.0.0.1", port=8000, http='h11', loop='asyncio')
+    uvicorn.run(
+        "practice_backend:app", host="127.0.0.1", port=8000, http="h11", loop="asyncio"
+    )
